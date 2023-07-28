7ec9486710cae193938736d63e6a296a047c2534
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-07-28 14:18:20 -0700

    Adds src/components/About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { image } from "../data/data";
+import image from "../data/data";
+// Assuming you have imported the 'image' variable here
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is a paragraph about myself. I love coding and building awesome web applications.</p>
+      <img src= "https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
